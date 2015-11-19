0b472676f299b60c6e5c2506d1f66e1d85aa6a08
Roger Bosman <Roger Bosman@DESKTOP-HADA3K0.home>
2015-11-19 17:40:20 +0100

    goede maven commig

---
+++ src/main/java/edu/groep6/ipsen3/Main.java
@@ -0,0 +1,15 @@
+package edu.groep6.ipsen3;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Created by Roger Bosman on 19-11-2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+        JSONObject jsonObject;
+        JSONArray jsonArray;
+    }
+}
