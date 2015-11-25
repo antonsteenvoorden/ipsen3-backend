e26a7a23ad3711dcde833e35c664f0458a3b8be7
Anton Steenvoorden <antons@live.nl>
2015-11-25 23:55:55 +0100

    Commit met SmartGit

--- src/main/java/edu/groep6/ipsen3/Main.java
+++ src/main/java/edu/groep6/ipsen3/Main.java
@@ -2,7 +2,7 @@ package edu.groep6.ipsen3;
 
 /**
  * Created by Roger Bosman on 19-11-2015.
- * Tetsten intelliJ
+ * Tetsten intelliJ met smartgit
  */
 public class Main {
 //    public static void main(String[] args) {
