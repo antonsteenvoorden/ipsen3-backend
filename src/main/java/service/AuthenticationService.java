36467da3ca6fe5e6a037cddca4b0dd5285e3b773
BuildTools <unconfigured@null.spigotmc.org>
2016-01-11 23:02:08 +0100

    Start for authentication

--- src/main/java/service/AuthenticationService.java
+++ src/main/java/service/AuthenticationService.java
@@ -1,47 +1,37 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package service;
-//
-//import com.google.common.base.Optional;
-//import io.dropwizard.auth.AuthenticationException;
-//import io.dropwizard.auth.Authenticator;
-//import io.dropwizard.auth.Authorizer;
-//import io.dropwizard.auth.basic.BasicCredentials;
-//import petersinspiratiepakket.actorius.model.User;
-//import petersinspiratiepakket.actorius.persistence.UserDAO;
-//
-///**
-// *
-// * @author Peter van Vliet
-// */
-//public class AuthenticationService implements Authenticator<BasicCredentials, User>, Authorizer<User>
-//{
-//    private final UserDAO userDAO;
-//
-//    public AuthenticationService(UserDAO userDAO)
-//    {
-//        this.userDAO = userDAO;
-//    }
-//
-//    @Override
-//    public Optional<User> authenticate(BasicCredentials credentials) throws AuthenticationException
-//    {
-//        User user = userDAO.getByEmailAddress(credentials.getUsername());
-//
-//        if (user != null && user.getPassword().equals(credentials.getPassword()))
-//        {
-//            return Optional.of(user);
-//        }
-//
-//        return Optional.absent();
-//    }
-//
-//    @Override
-//    public boolean authorize(User user, String roleName)
-//    {
-//        return user.hasRole(roleName);
-//    }
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package service;
+
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.basic.BasicCredentials;
+
+import java.util.Optional;
+
+/**
+ * @author Anton Steenvoorden
+ */
+public class AuthenticationService
+    implements Authenticator<BasicCredentials, Klant>, Authorizer<Klant> {
+    private final KlantDAO klantDAO;
+
+    public AuthenticationService(KlantDAO klantDAO) {
+        this.klantDAO = klantDAO;
+    }
+
+    @Override public Optional<Klant> authenticate(BasicCredentials credentials) throws AuthenticationException {
+        Klant klant = klantDAO.get(credentials.getUsername());
+
+        if (klant != null && klant.getPassword().equals(credentials.getPassword())) {
+            return Optional.of(klant);
+        }
+
+        return Optional.absent();
+    }
+
+    @Override public boolean authorize(Klant klant, String roleName) {
+        return klant.hasRole(roleName);
+    }
+}
