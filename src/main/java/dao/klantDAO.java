8d00f95fb1cd25037895afa58dada8cc7d19f2c6
BuildTools <unconfigured@null.spigotmc.org>
2016-01-18 23:12:40 +0100

    Update for klant, added PDFBox as prep for mail

--- src/main/java/dao/klantDAO.java
+++ src/main/java/dao/klantDAO.java
@@ -30,7 +30,7 @@ public interface KlantDAO {
           "INSERT INTO klant (klant_email, klant_voornaam, klant_tussenvoegsel, klant_achternaam, klant_straatnaam, " +
           "klant_huisnummer, klant_huisnummer_toevoeging,klant_postcode, klant_postcode_toevoeging, klant_plaatsnaam, "+
           "klant_telefoon, klant_gastlid, klant_notitie, klant_isactief ) VALUES (:email, :voornaam, :tussenvoegsel, " +
-          ":achternaam, :straatnaam, :huisNummer, :huisNummerToevoeging, :postcode, :postcodeToevoeging, :telefoon, " +
+          ":achternaam, :straatnaam, :huisNummer, :huisNummerToevoeging, :postcode, :postcodeToevoeging,:plaatsNaam, :telefoon, " +
           ":gastLid, :notitie, :isActief)" +
           "INSERT INTO account (klant_email,account_password, account_isklant, account_islid, account_isms, " +
           "account_isadmin, account_isactief)"+
@@ -38,6 +38,33 @@ public interface KlantDAO {
           "COMMIT;")
   void add(@BindBean Klant klant);
 
+  @SqlUpdate("BEGIN;" +
+                 "UPDATE klant "
+                 + "SET klant_email = :email, "
+                 + "klant_voornaam = :voornaam,"
+                 + "klant_tussenvoegsel = :tussenvoegsel,"
+                 + "klant_achternaam = :achternaam"
+                 + "klant_straatnaam = :straatnaam "
+                 + "klant_huisnummer = :huisNummer, "
+                 + "klant_huisnummer_toevoeging = :huisNummerToevoeging,"
+                 + "klant_postcode = :postcode, "
+                 + "klant_postcode_toevoeging = :postcodeToevoeging,"
+                 + "klant_plaatsnaam = :plaatsNaam, "
+                 + "klant_telefoon :telefoon, "
+                 + "klant_gastlid = :gastLid, "
+                 + "klant_notitie = :notitie, "
+                 + "klant_isactief = :isActief"
+                 + "WHERE klant_email = :email;"
+                 + "UPDATE account"
+                 + "SET klant_email = :email,"
+                 + "account_password = :password"
+                 + "account_isklant = :isKlant,"
+                 + "account_islid = :isLid,"
+                 + "account_isms = :isMS,"
+                 + "account_isadmin = :isAdmin,"
+                 + "account_isactief =:inMailingList"
+                 + "WHERE klant_email = :email;"
+                 + "COMMIT;")
   void update(@Bind("klant_email") String email, @BindBean Klant klant);
 
   void delete(@Bind("klant_email") String email);
